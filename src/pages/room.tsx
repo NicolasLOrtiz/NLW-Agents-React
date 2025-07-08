3001b2d80d25d98fb68cf40f5bd9cb8bba0f3eaf
NicolasLOrtiz <contato.nicolol@gmail.com>
2025-07-08 20:04:38 -0300

    initialize project with React, TypeScript, and Vite setup; add basic components and styles

---
+++ src/pages/room.tsx
@@ -0,0 +1,8 @@
+export const Room = () => {
+    return (
+        <div className="flex h-screen flex-col items-center justify-center bg-gray-100">
+            <h1 className="mb-4 font-bold text-3xl">Room Page</h1>
+            <p className="text-lg">This is the room page.</p>
+        </div>
+    )
+}
