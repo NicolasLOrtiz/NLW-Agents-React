3894a5b47ae5285f25acf38f19fd97cfff58eb85
NicolasLOrtiz <contato.nicolol@gmail.com>
2025-07-09 10:25:04 -0300

    Aula 03 - add RecordRoomAudio component; implement audio recording functionality and routing (35m)

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,6 @@
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {RecordRoomAudio} from '@/pages/record-room-audio.tsx'
 import {CreateRoom} from './pages/create-room.tsx'
 import {Room} from './pages/room.tsx'
 
@@ -12,6 +13,7 @@ export const App = () => {
                 <Routes>
                     <Route element={<CreateRoom/>} index/>
                     <Route element={<Room/>} path="/room/:roomId"/>
+                    <Route element={<RecordRoomAudio/>} path="/room/:roomId/audio"/>
                 </Routes>
             </BrowserRouter>
         </QueryClientProvider>
